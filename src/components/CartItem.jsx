1af00f919babd89e89fafc9a6898fbdfe3d547c4
AnjuIsOP <146796089+AnjuIsOP@users.noreply.github.com>
2026-04-26 09:30:02 +0530

    Add CartItem component for shopping cart display

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CartItem() {
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <p>Items added to cart will appear here.</p>
+    </div>
+  );
+}
+
+export default CartItem;
