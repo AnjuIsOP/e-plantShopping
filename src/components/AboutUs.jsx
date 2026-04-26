621db9020953fb1a567a87784adaced90ddbc795
AnjuIsOP <146796089+AnjuIsOP@users.noreply.github.com>
2026-04-26 09:25:25 +0530

    Add AboutUs component for nursery information

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Welcome to Paradise Nursery, your one-stop destination for beautiful indoor and outdoor plants.
+        We aim to bring nature closer to your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
